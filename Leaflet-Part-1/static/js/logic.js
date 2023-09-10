7c215ac1845fe5c6fed339cb1aeb3792b54b64e3
evpenguin <evangeline.rowney@gmail.com>
2023-09-10 19:43:29 +1000

    final code

--- Leaflet-Part-1/static/js/logic.js
+++ Leaflet-Part-1/static/js/logic.js
@@ -9,7 +9,7 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
   attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(myMap);
 
-let url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/2.5_day.geojson";
+let url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_month.geojson";
 
 //get data
 d3.json(url).then(function(response) {
@@ -25,25 +25,18 @@ d3.json(url).then(function(response) {
         // Set the marker radius for the city by passing the population to the markerSize() function.
         earthquakes.push(
           L.circle([data[i].geometry.coordinates[1], data[i].geometry.coordinates[0]], {
-            stroke: false,
-            fillOpacity: 0.75,
-            //color: , //chloropleth function,//
-            //fillColor: , //chloropleth function,
-            radius: data[i].geometry.coordinates[2]*1000
-          })
+            stroke: true,
+            weight: 1,
+            fillOpacity: 1,
+            color: "black",
+            fillColor: markerColour(data[i].geometry.coordinates[2]),
+            radius: markerSize(data[i].properties.mag)
+          }).bindPopup(`<p>Location: ${data[i].properties.place}</p>
+            <p>Magnitude: ${data[i].properties.mag}</p>
+            <p>Depth: ${data[i].geometry.coordinates[2]}km`)
         );
       };
       
-      // Create the base layers.
-      let street = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-          attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-      });
-      
-      let topo = L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
-          attribution: 'Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)'
-      });
-      
-      // Create two separate layer groups: one for the city markers and another for the state markers.
       let markers = L.layerGroup(earthquakes);
       
       // Create an overlay object.
@@ -55,13 +48,48 @@ d3.json(url).then(function(response) {
       // Add the layer control to the map.
       markers.addTo(myMap);
       
-  
+
+  let legend = L.control({ position: "bottomright" });
+  legend.onAdd = function() {
+    let div = L.DomUtil.create("div", "info legend");
+    let labels = ["-10 - 10", "10 - 30", "30 - 50", "50 - 70", "70 - 90", "90+"];
+      
+    let legendInfo = ``
+    // Add the minimum and maximum.
+    for (i = 0; i < colours.length; i++ ) {
+      legendInfo += `<i style="background:${colours[i]}"></i><span>${labels[i]}</span><br>`
+    }
+    div.innerHTML = legendInfo;
+    return div;
+  };
+      
+  // Adding the legend to the map
+  legend.addTo(myMap);
 });
 
 function markerSize(magnitude) {
-    return Math.sqrt(magnitude) * 150;
+    return (magnitude*50000)
 };
 
+let colours = ["#ffb56b", "#e16b5d", "#ca485c", "#ac255e", "#5b1060", "#1f005c"]
+
 function markerColour(depth) {
-    
+    if(depth < 10) {
+      return colours[0]
+    }
+    else if( depth < 30) {
+      return colours[1]
+    }
+    else if (depth < 50) {
+      return colours[2]
+    }
+    else if (depth < 70) {
+      return colours[3]
+    }
+    else if (depth < 90) {
+      return colours[4]
+    }
+    else {
+      return colours[5]
+    }
 }
