1a7875ada3bec3dc3c8816ae313b281980299411
evpenguin <evangeline.rowney@gmail.com>
2023-09-10 20:27:28 +1000

    final comments

--- Leaflet-Part-1/static/js/logic.js
+++ Leaflet-Part-1/static/js/logic.js
@@ -9,6 +9,7 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
   attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(myMap);
 
+//data
 let url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_month.geojson";
 
 //get data
@@ -22,7 +23,7 @@ d3.json(url).then(function(response) {
       // Loop through locations, and create the city and state markers.
       for (let i = 0; i < data.length; i++) {
       
-        // Set the marker radius for the city by passing the population to the markerSize() function.
+        // Set up markers
         earthquakes.push(
           L.circle([data[i].geometry.coordinates[1], data[i].geometry.coordinates[0]], {
             stroke: true,
@@ -37,25 +38,25 @@ d3.json(url).then(function(response) {
         );
       };
       
+      //create layer group for the markers
       let markers = L.layerGroup(earthquakes);
       
       // Create an overlay object.
       let overlayMaps = {
         "Earthquakes": markers
       };
-      
-      // Pass our map layers to our layer control.
-      // Add the layer control to the map.
+      //add to the map
       markers.addTo(myMap);
       
 
+  //create legend
+  //adapted from https://codepen.io/haakseth/pen/KQbjdO
   let legend = L.control({ position: "bottomright" });
   legend.onAdd = function() {
     let div = L.DomUtil.create("div", "info legend");
     let labels = ["-10 - 10", "10 - 30", "30 - 50", "50 - 70", "70 - 90", "90+"];
       
     let legendInfo = ``
-    // Add the minimum and maximum.
     for (i = 0; i < colours.length; i++ ) {
       legendInfo += `<i style="background:${colours[i]}"></i><span>${labels[i]}</span><br>`
     }
@@ -67,12 +68,15 @@ d3.json(url).then(function(response) {
   legend.addTo(myMap);
 });
 
+//caculates the size of the marker based on the magnitude
 function markerSize(magnitude) {
     return (magnitude*50000)
 };
 
+//list of the colours 
 let colours = ["#ffb56b", "#e16b5d", "#ca485c", "#ac255e", "#5b1060", "#1f005c"]
 
+//calculates the colour of each marker based on depth
 function markerColour(depth) {
     if(depth < 10) {
       return colours[0]
